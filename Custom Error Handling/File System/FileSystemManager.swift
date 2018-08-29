8a0e6e3c8b5a285fbe5061ca9dce2ef332cab84a
Mark Filter <markfilter@outlook.com>
2018-08-29 16:22:59 -0400

    Adds gitignore file

---
+++ Custom Error Handling/File System/FileSystemManager.swift
@@ -0,0 +1,9 @@
+//
+//  FileSystemManager.swift
+//  Custom Error Handling
+//
+//  Created by Mark Filter on 8/29/18.
+//  Copyright © 2018 MarkZFilter.com. All rights reserved.
+//
+
+import Foundation
--- Custom Error Handling/Main/MainController.swift
+++ Custom Error Handling/Main/MainController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainController.swift
 //  Custom Error Handling
 //
 //  Created by Mark Filter on 8/29/18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
