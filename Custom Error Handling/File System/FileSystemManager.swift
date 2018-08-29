c358bff729363cbe3f334541195e98ca8013dc00
Mark Filter <markfilter@outlook.com>
2018-08-29 16:25:43 -0400

    Adds project files to repo

--- Custom Error Handling/File System/FileSystemManager.swift
+++ Custom Error Handling/File System/FileSystemManager.swift
@@ -1,5 +1,5 @@
 //
-//  FileSystemManager.swift
+//  FileSystemClient.swift
 //  Custom Error Handling
 //
 //  Created by Mark Filter on 8/29/18.
@@ -7,3 +7,51 @@
 //
 
 import Foundation
+
+enum FileSystemError: Error {
+    case fileIOException(String)
+    case saveToFileUnsuccessful(String)
+}
+
+class FileSystemClient {
+    
+    public static let shared = FileSystemClient()
+    
+    func loadFile(with url: String, fileExtension: String) throws -> Codable {
+        
+        guard let fileUrl = Bundle.main.url(forResource: url, withExtension: fileExtension) else {
+            throw FileSystemError.fileIOException("File was not found with given filename.")
+        }
+        
+        guard let contentsOfFile =  try? String.init(contentsOf: fileUrl, encoding: String.Encoding.utf8) else {
+            throw FileSystemError.fileIOException("File was empty.")
+        }
+        
+        if contentsOfFile == "" {
+            throw FileSystemError.fileIOException("File was empty.")
+        }
+        
+        let decoder = JSONDecoder()
+        guard let fileObject = try? decoder.decode(FileContents.self, from: contentsOfFile.data(using: String.Encoding.utf8)!) else {
+            throw FileSystemError.fileIOException("File contents were corrupt.")
+        }
+        
+        return fileObject
+    }
+    
+    func save(contents: String, to url: String, withExtention: String) throws -> Void {
+        guard let fileUrl = Bundle.main.url(forResource: url, withExtension: withExtention) else {
+            throw FileSystemError.fileIOException("File was not found with given filename.")
+        }
+        do {
+            let encoder = JSONEncoder()
+            let fileContents = FileContents(secretPassword: contents)
+            let data = try! encoder.encode(fileContents)
+            let contentString = String(data: data, encoding: .utf8)!
+            try contentString.write(to: fileUrl, atomically: true, encoding: String.Encoding.utf8)
+        } catch {
+            throw FileSystemError.saveToFileUnsuccessful("There was an error while attempting to save to \(url + "." + withExtention)")
+        }
+    }
+    
+}
--- Custom Error Handling/Main/MainController.swift
+++ Custom Error Handling/Main/MainController.swift
@@ -8,18 +8,118 @@
 
 import UIKit
 
-class MainController: UIViewController {
+struct FileContents: Codable {
+    var secretPassword: String? = nil
+}
 
+class MainController: UIViewController {
+    
+    // MARK: - Injections
+    var fileSystemClient = FileSystemClient.shared
+    // Properties
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var errorOutputLabel: UILabel!
+    @IBOutlet var secretPasswordLabel: UILabel!
+    
+    // MARK: - Lifecycle Callbacks
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        secretPasswordLabel.text = ""
+        do {
+            try fileSystemClient.save(contents: "MarkZFilter.com", to: "super-secret-password", withExtention: "txt")
+            clearErrorMessage()
+            
+        } catch FileSystemError.fileIOException(let message) {
+            errorOutputLabel.text = message
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func clearErrorMessage() {
+        errorOutputLabel.text = ""
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - Actions
+    
+    @IBAction func handlerSaveButtonTapped(_ sender: UIButton) {
+        do {
+            try fileSystemClient.save(contents: passwordTextField?.text ?? "", to: "super-secret-password", withExtention: "txt")
+            clearErrorMessage()
+            
+        } catch FileSystemError.fileIOException(let message) {
+            errorOutputLabel.text = message
+        } catch {
+            print(error.localizedDescription)
+        }
+        passwordTextField?.text = ""
     }
+    
+    
+    @IBAction func handlerLoadProperFileTapped(_ sender: UIButton) {
+        do {
+            
+            guard let contents = try fileSystemClient.loadFile(with: "super-secret-password", fileExtension: "txt") as? FileContents else {
+                return
+            }
 
-
+            secretPasswordLabel.text = contents.secretPassword
+            clearErrorMessage()
+        } catch FileSystemError.fileIOException(let message) {
+            errorOutputLabel.text = message
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    @IBAction func handlerLoadCorruptedFileTapped(_ sender: UIButton) {
+        do {
+            
+            guard let contents = try fileSystemClient.loadFile(with: "corrupted-file", fileExtension: "txt") as? FileContents else {
+                return
+            }
+            
+            secretPasswordLabel.text = contents.secretPassword
+            clearErrorMessage()
+        } catch FileSystemError.fileIOException(let message) {
+            errorOutputLabel.text = message
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    
+    @IBAction func hanlderLoadEmptyFileTapped(_ sender: UIButton) {
+        do {
+            guard let contents = try fileSystemClient.loadFile(with: "super-secret-password-empty", fileExtension: "txt") as? FileContents else {
+                return
+            }
+            secretPasswordLabel.text = contents.secretPassword
+            clearErrorMessage()
+        } catch FileSystemError.fileIOException(let message) {
+            errorOutputLabel.text = message
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    
+    @IBAction func handlerWrongFileTapped(_ sender: UIButton) {
+        do {
+            guard let contents = try fileSystemClient.loadFile(with: "name", fileExtension: "txt") as? FileContents else {
+                return
+            }
+            secretPasswordLabel.text = contents.secretPassword
+            clearErrorMessage()
+        } catch FileSystemError.fileIOException(let message) {
+            errorOutputLabel.text = message
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    
 }
 
